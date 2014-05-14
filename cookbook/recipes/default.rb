2bd2a57e646687773a3e8d5ddb329c485d984e2f
alexpop <alex@rightscale.com>
2014-05-14 19:03:11 +0000

    rollback

--- cookbook/recipes/default.rb
+++
@@ -1,8 +0,0 @@
-#
-# Cookbook Name:: rackspace_cloud
-# Recipe:: default
-#
-# Copyright 2014, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
--- recipes/wait_for_rackconnect.rb
+++ recipes/wait_for_rackconnect.rb
@@ -9,22 +9,24 @@
 
 rightscale_marker :begin
 
-touch /root/.noupdate
-touch /etc/rackspace/.bootstrapped
-touch /etc/rackspace/.noupdates
- 
-#causing yum lock issue #2
-#chkconfig --del yum-cron
-#rm -rf /etc/cron.daily/yum.cron
-#rm -rf /var/lock/subsys/yum-cron
- 
-#wait until the rackspace post install boot sequence has completed
-STATUS=`curl https://ord.api.rackconnect.rackspace.com/v1/automation_status?format=text`
-while  [ "$STATUS" != "DEPLOYED" ]; do
-   echo " waiting for rackspace post install to complete"
-   echo "current status: $STATUS"
-   STATUS=`curl https://ord.api.rackconnect.rackspace.com/v1/automation_status?format=text`
-   sleep 5
-done
+bash "Waiting for Rackconnect" do
+  flags "-ex"
+  user "root"
+  cwd "/tmp"
+  code <<-EOH
+	touch /root/.noupdate
+	touch /etc/rackspace/.bootstrapped
+	touch /etc/rackspace/.noupdates
+
+	#wait until the rackspace post install boot sequence has completed
+	STATUS=`curl https://ord.api.rackconnect.rackspace.com/v1/automation_status?format=text`
+	while  [ "$STATUS" != "DEPLOYED" ]; do
+	   echo " waiting for rackspace post install to complete"
+	   echo "current status: $STATUS"
+	   STATUS=`curl https://ord.api.rackconnect.rackspace.com/v1/automation_status?format=text`
+	   sleep 5
+	done
+  EOH
+end
 
 rightscale_marker :end
