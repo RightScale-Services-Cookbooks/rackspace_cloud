c0c0160f86227c25fb7a3915bf59a3f406e798f3
Alex Pop <alex@rightscale.com>
2014-05-16 12:49:49 +0100

    add log file

--- recipes/wait_for_rackconnect.rb
+++ recipes/wait_for_rackconnect.rb
@@ -1,3 +1,4 @@
+	
 #
 # Cookbook Name:: rackspace_cloud
 # Recipe:: default
@@ -9,19 +10,24 @@
 
 rightscale_marker :begin
 
-`
-  touch /root/.noupdate
-  touch /etc/rackspace/.bootstrapped
-  touch /etc/rackspace/.noupdates
+log "wait_for_rackconnect logs are found in /var/log/wait_for_rackconnect.log"
 
-  #wait until the rackspace post install boot sequence has completed
-  STATUS=system('curl https://ord.api.rackconnect.rackspace.com/v1/automation_status?format=text')
-  while  [ "$STATUS" != "DEPLOYED" ]; do
-    echo " waiting for rackspace post install to complete"
-    echo "current status: $STATUS"
-    STATUS=system('curl https://ord.api.rackconnect.rackspace.com/v1/automation_status?format=text')
-    sleep 5
-  done
-`
+system('
+  log_file="/var/log/wait_for_rackconnect.log"
+  if [ -d "/etc/rackspace" ]; then
+    touch /root/.noupdate
+    touch /etc/rackspace/.bootstrapped
+    touch /etc/rackspace/.noupdates
+
+    #wait until the rackspace post install boot sequence has completed
+    STATUS=`curl https://ord.api.rackconnect.rackspace.com/v1/automation_status?format=text`
+    while  [ "$STATUS" != "DEPLOYED" ]; do
+      echo " waiting for rackspace post install to complete"
+      echo "current status: $STATUS"
+      STATUS=`curl https://ord.api.rackconnect.rackspace.com/v1/automation_status?format=text`
+      sleep 5
+    done
+  fi > $log_file 2>&1
+')
 
 rightscale_marker :end
