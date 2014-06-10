4f14661968d64dd809084182d5cbc44f46d58deb
Alex Pop <alex@rightscale.com>
2014-06-10 09:09:42 +0100

    added rackspace region attribute

--- metadata.rb
+++ metadata.rb
@@ -8,3 +8,14 @@ version          '0.1.0'
 
 recipe "rackspace_cloud::wait_for_rackconnect",
   "Wait for the RackConnect automation to avoid yum install conflict"
+  
+attribute "rackspace_cloud/region",
+  :display_name => "Rackspace Cloud Region",
+  :description =>
+    "Specify the cloud region or data center being used for this service." +
+    " Example: ORD (Chicago)",
+  :required => "optional",
+  :default => "ORD (Chicago)",
+  :choice => ["ORD (Chicago)", "DFW (Dallas/Ft. Worth)", "IAD (Northern Virginia)", "LON (London)", "SYD (Sydney)", "HKG (Hong Kong)"],
+  :recipes => [ "rackspace_cloud::wait_for_rackconnect" ]
+  
