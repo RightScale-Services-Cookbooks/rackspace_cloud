d7c71c9298d3eb3f052a1f9731fe0f55dd89a71d
Alex Pop <alex@rightscale.com>
2014-06-10 09:33:20 +0100

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -6,8 +6,10 @@ description      'Rackspace cloud utilities'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends "rightscale"
+
 recipe "rackspace_cloud::wait_for_rackconnect",
-  "Wait for the RackConnect automation to avoid yum install conflict"
+  "Wait for the RackConnect automation to avoid a yum install conflict"
   
 attribute "rackspace_cloud/region",
   :display_name => "Rackspace Cloud Region",
