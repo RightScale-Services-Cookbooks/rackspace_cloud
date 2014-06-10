6522c9b033b1014c7e31aa7510be228e42443ffd
Alex Pop <alex@rightscale.com>
2014-06-10 09:48:34 +0100

    Update wait_for_rackconnect.rb

--- recipes/wait_for_rackconnect.rb
+++ recipes/wait_for_rackconnect.rb
@@ -14,20 +14,31 @@ log "wait_for_rackconnect logs are found in /var/log/wait_for_rackconnect.log"
 
 # Need to run this in the compile phase, hence the system call
 %x[
-  echo "*** Here we go in bash!"
-  rackspace_region="#{node[:rackspace_cloud][:region]}"
-  echo "*** rackspace_region=$rackspace_region"
   log_file="/var/log/wait_for_rackconnect.log"
   if [ -d "/etc/rackspace" ]; then
     touch /root/.noupdate
     touch /etc/rackspace/.bootstrapped
     touch /etc/rackspace/.noupdates
+    
+    echo "*** /root/rackconnectuserconfig.log contains:"
+    cat /root/rackconnectuserconfig.log
 
     echo "*** yum processes start:"
     ps aux | grep yum | grep -v "grep yum"
+    
+    rackspace_region="#{node[:rackspace_cloud][:region]}"
+    rackspace_region=`sed -r 's/(\w+).*/\1/'<<<$rackspace_region | tr '[:upper:]' '[:lower:]'`
+    echo "*** rackspace_region=$rackspace_region"
+
+    if [ -z "$rackspace_region" ] ; then 
+      echo "*** ERROR: Invalid RackSpace region"
+      exit -1
+    fi
+
+    region_url="https://$rackspace_region.api.rackconnect.rackspace.com/v1/automation_status?format=text"
+    
     #wait until the rackspace post install boot sequence has completed
-    date
-    STATUS=`curl https://ord.api.rackconnect.rackspace.com/v1/automation_status?format=text`
+    STATUS=`curl $region_url`
     echo "*** current status: $STATUS"
     while  [ "$STATUS" != "DEPLOYED" ]; do
       echo '*** waiting for rackspace post install to complete'
