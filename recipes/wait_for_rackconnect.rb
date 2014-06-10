d7006e827ebd770767c3d8e0b2232b270dfd54a8
Alex Pop <alex@rightscale.com>
2014-06-10 09:16:46 +0100

    more troubleshooting needed

--- recipes/wait_for_rackconnect.rb
+++ recipes/wait_for_rackconnect.rb
@@ -12,7 +12,11 @@ rightscale_marker :begin
 
 log "wait_for_rackconnect logs are found in /var/log/wait_for_rackconnect.log"
 
-system('
+# Need to run this in the compile phase, hence the system call
+%x[
+  echo "*** Here we go in bash!"
+  rackspace_region="#{node[:rackspace_cloud][:region]}"
+  echo "*** rackspace_region=$rackspace_region"
   log_file="/var/log/wait_for_rackconnect.log"
   if [ -d "/etc/rackspace" ]; then
     touch /root/.noupdate
@@ -26,13 +30,13 @@ system('
     STATUS=`curl https://ord.api.rackconnect.rackspace.com/v1/automation_status?format=text`
     echo "*** current status: $STATUS"
     while  [ "$STATUS" != "DEPLOYED" ]; do
-      echo "*** waiting for rackspace post install to complete"
+      echo '*** waiting for rackspace post install to complete'
       echo "*** current status: $STATUS"
       STATUS=`curl https://ord.api.rackconnect.rackspace.com/v1/automation_status?format=text`
     done
     echo "*** yum processes end:"
     ps aux | grep yum | grep -v "grep yum"
   fi > $log_file 2>&1
-')
+]
 
 rightscale_marker :end
