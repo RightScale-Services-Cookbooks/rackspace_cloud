4ae6fc30141de05db681eceb959589cff4669144
Richard Shade <rshade@rightscale.com>
2015-02-25 17:22:07 -0600

    adding providers

---
+++ Berksfile
@@ -0,0 +1,5 @@
+site :opscode
+
+metadata
+
+cookbook 'fog', github: 'RightScale-Services-Cookbooks/fog', branch: master
--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email 'support@rightscale.com'
 license          'All rights reserved'
 description      'Rackspace cloud utilities'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
-depends "rightscale"
+depends "fog"
 
 recipe "rackspace_cloud::wait_for_rackconnect",
   "Wait for the RackConnect automation to avoid a yum install conflict"
