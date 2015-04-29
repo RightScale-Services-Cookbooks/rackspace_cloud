995bb5695b94a0e910b9fb420797d490fedfaa1f
Daniel Wallace <danielwallace@gtmanfred.com>
2015-04-29 12:34:39 -0500

    update version in metadata

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'support@rightscale.com'
 license          'All rights reserved'
 description      'Rackspace cloud utilities'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
 
 depends "fog"
 depends "marker"
